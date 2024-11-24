374c08e1c4b63aaa0f85dd2ef558b3e5f38ce8f1
soumya Hegde <soumyahegde652@gmail.com>
2024-11-24 20:00:32 +0530

    taking inputs with different data variable type

---
+++ Inputs.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Inputs {
+    public static void main (String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        // sc.useRadix(2);
+        // System.out.println("enter the binary number");
+        // int x=sc.nextInt();
+        // System.out.println(x);
+
+        // System.out.println("sum of two nums:");
+        // System.out.println("enter two numbers");
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int c= a + b;
+        // System.out.println(c);
+
+        System.out.println("greeting message");
+        System.out.println("enter the name");
+        String name= sc.nextLine();
+        System.out.println("hello namaste " + name );
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
