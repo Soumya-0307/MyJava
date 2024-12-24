835c172a087dbb09d5186d07a82bdb74e94c04b5
soumya Hegde <soumyahegde652@gmail.com>
2024-12-24 13:01:06 +0530

    what is single literals means

---
+++ Literals.java
@@ -0,0 +1,19 @@
+public class Literals {
+    public static void main(String args[]){
+        int amt=678;
+        long sal=123789;
+        char c=0X0370; // it is an greek unicode to represnt it in hexa decimal.
+        int y=5;
+
+        System.out.println(amt);
+        System.out.println(sal);
+        System.out.println(c);
+        System.out.println(Integer.toBinaryString(y));
+
+
+
+
+
+
+    }
+}
