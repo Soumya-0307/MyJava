43fca425ce3a38c842106c13e71104277da2b245
soumya Hegde <soumyahegde652@gmail.com>
2025-01-03 21:41:49 +0530

    String data type and object

---
+++ Stringss.java
@@ -0,0 +1,14 @@
+public class Stringss {
+    public static void main(String args[]){
+        char[] c={'a','b','c','d'};
+        String str=new String(c);
+        System.out.println(str);
+
+        byte[] b={65,66,67,68};
+        String str2=new String(b);
+        System.out.println(str2);
+
+
+
+    }
+}
