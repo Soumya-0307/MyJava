303001a8be155f90b29bb380b3b2cfed78f9636f
soumya Hegde <soumyahegde652@gmail.com>
2025-01-25 12:54:53 +0530

    multiplication table

---
+++ MulTable.java
@@ -0,0 +1,10 @@
+public class MulTable{
+    public static void main(String[] args) {
+        System.out.println("multiplication tabel of 5:");
+        for(int i=1;i<=10;i++){
+            System.out.println(5+"*"+i+"="+5*i);
+        }
+    
+    }
+
+}
